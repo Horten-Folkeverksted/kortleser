c3831f9228f6578e538ce5ebb4faae8e1475e445
Daniel Olsen <daniel.olsen99@gmail.com>
2020-09-21 20:23:42 +0200

    Add disaraging comments

--- src/main.rs
+++ src/main.rs
@@ -38,41 +38,41 @@ fn convert(strings: Vec<&str>) -> Result<(CSN, PACS), hex::FromHexError> {
 
 fn handle_client(mut stream: TcpStream, r: multiqueue::BroadcastReceiver<u64>) {
     loop {
-        let message = match r.try_recv() {
+        let message = match r.try_recv() {                                 // Recieve a message maybe, shitty library doesnt have a blocking one...
             Ok(val) => {
                 val
             },
-            Err(_) => {
-                continue;
+            Err(e) => {
+                println!("Error: {}", e)
+                continue;                                                   // If we didnt recieve anything I guess we just try again lol
             },
         };
 
-        stream.write(&message.to_string()[..].as_bytes());
-        stream.write("\n".as_bytes());
+        stream.write(&message.to_string()[..].as_bytes());                  // stream write takes a slice of u8s, but our hash is a u64. essentially turning it back to a string now
+        stream.write("\n".as_bytes());                                      // dont even want to attempt adding a char to the slice...
     }
-}
-
-
+}                                                                           // If you havent been paying attention, architecture is as follows:
+                                                                            //  card data -> keypresses over emulated usb kb -> read as str from a buffer
+                                                                            //    -> converted to byte arrays -> hashed -> converted to String -> forced into a str slice -> into a byte array.
 fn main() {
     let stdin = io::stdin();
+    // This number is 10 because ¯\_(ツ)_/¯
     let (s, r) = multiqueue::broadcast_queue::<u64>(10);
     thread::spawn(move|| {
         for line in stdin.lock().lines() {
-            let text = line.unwrap();
-    
-            let strings: Vec<&str> = text.split("\t").take(2).collect();
-    
-            if strings.len() != 2 {
+            let text = line.unwrap();                                       // Read the line
+            let strings: Vec<&str> = text.split("\t").take(2).collect();    // Split into its elements
+            if strings.len() != 2 {                                         // Check if its two and not one or zero...
                 println!("not two tab separated elements");
                 continue;
             };
-            let data = convert(strings).unwrap();
+            let data = convert(strings).unwrap();                           // convert the strings to byte arrays because ???
             
-            let mut hasher = DefaultHasher::new();
-            data.hash(&mut hasher);
+            let mut hasher = DefaultHasher::new();                          // Hash it using some internal hashing mechanism I dont know anything about
+            data.hash(&mut hasher);                                         // Probably reversible for all I know
             let hash = hasher.finish();
 
-            s.try_send(hash);
+            s.try_send(hash);                                               // Send it with some weird channel library that let me do SPMC with broadcasting
         }
     });
 
