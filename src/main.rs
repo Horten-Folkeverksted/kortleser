7aa56d129a2b81d38227cc03713f01484da99ee0
Daniel Olsen <daniel.olsen99@gmail.com>
2020-09-22 09:57:34 +0200

    add license

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
+// kortleser reads information from stdin and sends it to connected tcp clients
+// Copyright (C) 2020  Daniel Løvbrøtte Olsen
+
+// This program is free software: you can redistribute it and/or modify 
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+
 use std::sync::{Arc, Mutex};
 
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
-
-
 
 use std::io::{self, BufRead, Write};
 use std::net::{TcpListener, TcpStream};
