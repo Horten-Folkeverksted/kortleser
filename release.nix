bf48d4d65cce4fb1d67b47a27e8272f35070474c
Daniel Olsen <daniel.olsen99@gmail.com>
2021-05-04 21:27:26 +0200

    dont build shell

--- release.nix
+++ release.nix
@@ -1,3 +1,5 @@
 { nixpkgs ? <nixpkgs>, moz_overlay ? <moz_overlay> }:
-
-import ./default.nix {inherit nixpkgs moz_overlay; }
+with (import ./default.nix {inherit nixpkgs moz_overlay; });
+{
+  inherit kortleser kortleserRpi image;
+}
